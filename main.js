18b7877ce460dc2b314e15de48658f89f1756d75
IsaMLeal123 <isabelle.menezes.j96507@mcast.edu.mt>
2026-01-15 21:38:00 +0100

    A whole day and I still did not manage to do the banner properly
    I'm going to cry in Java Script now

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".carousel").forEach(carousel =>{
+    const items = carousel.querySelectorAll(".carousel-item");
+    const buttonsHtml = Array.from(items, () => {
+        return `<span class="carousel-button"></span>`;
+    });
+
+    carousel.insertAdjacentHTML("beforeend", `
+        
+        `);
+});
